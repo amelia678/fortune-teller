faa69005700ef55a23b354bd8b33e82fe9486543
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-10-11 10:42:00 -0400

    adds fortune array

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,14 @@ console.log('yep i am here');
 // #1 make some code run when i click the button
 const triggerElement = document.querySelector('[data-trigger]');
 const outputElement = document.querySelector('[data-output]');
+
+const fortunes = [
+    "You will be hungry again in one hour",
+    "I wish things had ended differently",
+    "Even a broken clock is right two times a day",
+    "Your resemblence to a muppet will prevent the world from taking you seriously",
+    "Never gonna give you up, never gonna let you down, never gonna run around and dessert you"
+];
 // function hello() {
 //     ;
 // } if function will only be used once, ok to use anonymous function
@@ -14,7 +22,7 @@ const outputElement = document.querySelector('[data-output]');
 //  -a function that says what to do when the event happens
 triggerElement.addEventListener('click', function () {
     console.log('Hello addEventListener!');
-    outputElement.textContent = 'grumpy grumpy grumpy cat';
+    outputElement.textContent = fortunes.pop();
 });    
 
 // #2 write some text to the page on button click
