19ccb681e1ac813b4f8f9fc46bcbee6fa9505f64
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-10-11 10:32:49 -0400

    sets test output content

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,20 @@ console.log('yep i am here');
 
 // #1 make some code run when i click the button
 const triggerElement = document.querySelector('[data-trigger]');
-
-function hello() {
-    console.log('Hello addEventListener!');
-}
+const outputElement = document.querySelector('[data-output]');
+// function hello() {
+//     ;
+// } if function will only be used once, ok to use anonymous function
 
 // addEventListener accepts 2 arguments:
 // - a string that identifies the kind of event to listen for
 //  -a function that says what to do when the event happens
-triggerElement.addEventListener('click', hello);
+triggerElement.addEventListener('click', function () {
+    console.log('Hello addEventListener!');
+    outputElement.textContent = 'grumpy grumpy grumpy cat';
+});    
 
 // #2 write some text to the page on button click
+
 
 // #3 pull text from an array of text to write to the page.
