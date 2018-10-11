45ba934801990b3308a852d63d37feab81ab32b0
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-10-11 13:47:25 -0400

    adds random element

--- scripts/index.js
+++ scripts/index.js
@@ -24,9 +24,10 @@ let index = 0;
 // addEventListener accepts 2 arguments:
 // - a string that identifies the kind of event to listen for
 //  -a function that says what to do when the event happens
+// let randomFortune = fortunes[Math.floor(Math.random()* fortunes.length)];
 triggerElement.addEventListener('click', function () {
     console.log('Hello addEventListener!');
-    const newFortune = fortunes[index];
+    let newFortune = fortunes[Math.floor(Math.random()* fortunes.length)];
     outputElement.textContent= newFortune;
     index++;
     if (index > fortunes.length - 1) {
@@ -43,7 +44,7 @@ const proTips = [
     "Save money on Christmas presents by discussing politics on social media",
     "If somebody you know has a weird laugh, tell them so they know not to laugh again",
     "Speak twice before you think",
-    "If you can't think of a word, say I forget the English word for it. That way people will think you're bilingual instead of an idiot",
+    "If you can't think of a word, say 'I forget the English word for it'. That way people will think you're bilingual instead of an idiot",
     "If your car is making weird noises just turn the music volume up and will no longer be a problem",
     "Slow blink every 3 seconds to effortlessly spread your sleep out through the day"
 
@@ -52,7 +53,7 @@ const proTips = [
 
 triggerElement2.addEventListener('click', function () {
     console.log('Guten tag');
-    const newLifeTip = proTips[index];
+    let newLifeTip = proTips[Math.floor(Math.random()*proTips.length)];
     outputElement.textContent = newLifeTip
     index++;
     if (index > proTips.length - 1) {
